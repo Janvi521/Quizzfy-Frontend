e209ed3a3084bb1fa4968e89e31486beeb8038b7
Janvi patel <janvip9123@gmail.com>
2025-09-18 07:54:33 +0530

    Update Home.jsx

--- src/comopents/home/Home.jsx
+++ src/comopents/home/Home.jsx
@@ -1,14 +1,14 @@
 import Footer from "../footer/Footer";
 import Navbar from "../navbar/Navbar"
 import Header from "../header/header";
-import Categories  from "../categery/Categery";
+// import Categories  from "../categery/Categery";
 import Features from "../features/Features";
 import HomeNavbar from "../navbar/HomeNavbar";
 function Home(){
 return <>
      <HomeNavbar/>
       <Header/>
-      <Categories/>
+{/*       <Categories/> */}
       <Features/>
       <Footer />
 </>
